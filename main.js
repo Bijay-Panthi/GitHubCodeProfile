07b32ec61b57d544b0254f80d7d6107f13b6e0be
Bijay Panthi <bijaypanthi@gmail.com>
2017-06-13 11:46:32 +1000

    Added files

---
+++ main.js
@@ -0,0 +1,19 @@
+//Setting the dimensions and margin
+
+var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+// Create a SVG Canvas
+
+var gitHubRepo = "https://api.github.com/users/" + username + "/repos";
+
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Getting Data from the Api
