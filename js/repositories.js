6f42381d3772715aa2c613f21edbd221bed2a200
Bijay Panthi <bijaypanthi@gmail.com>
2017-06-15 15:39:35 +1000

    Rendering repo with d3 part 2

--- js/repositories.js
+++ js/repositories.js
@@ -4,4 +4,15 @@ var url = "https://api.github.com/repos/" + repoName;
 
 d3.json(url, function(error, data) {
   console.log(data);
+  $("#reponame").text("Repo name: " + data.name);
+  $("#created").text("Date  Created: " + data.created_at);
+  $("#lastupdated").text("Last Updated: " + data.updated_at);
+  $("#size").text("Size : " + data.size);
 });
+
+// <h2 id="reponame">Loading...</h2>
+// <h4 id="lastupdated">Loading...</h4>
+// <p id="followers">Loading...</p>
+// <p id="following">Loading...</p>
+// <p id="public_repos">Loading...</p>
+// <p id="public_gists">Loading...</p>
